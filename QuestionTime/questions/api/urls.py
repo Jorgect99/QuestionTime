da9282138446671a3a31a779e01c7994b0010167
Jorgect99 <jorgect99@gmail.com>
2021-02-26 22:48:55 -0700

    Like and Detail Endpoints

--- QuestionTime/questions/api/urls.py
+++ QuestionTime/questions/api/urls.py
@@ -1,7 +1,6 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-
 from questions.api import views as qv
 
 router = DefaultRouter()
@@ -10,6 +9,12 @@ router.register(r"questions", qv.QuestionViewSet)
 
 urlpatterns = [
     path("", include(router.urls)), 
-    path("questions/<slug:slug>/answer/", qv.AnswerCreateAPIView.as_view(), name="create-answer"), 
-    path("questions/<slug:slug>/answers/", qv.AnswerListAPIView.as_view(), name="create-list"), 
+
+    path("questions/<slug:slug>/answer/", qv.AnswerCreateAPIView.as_view(), name="answer-create"),
+
+    path("questions/<slug:slug>/answers/", qv.AnswerListAPIView.as_view(), name="answer-list"),
+
+    path("answers/<int:pk>/", qv.AnswerRUDAPIView.as_view(), name="answer-detail"), 
+    
+    path("answers/<int:pk>/like/", qv.AnswerLikeAPIView.as_view(), name="answer-like"), 
 ]
